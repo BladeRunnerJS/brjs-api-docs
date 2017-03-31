e096136b0d21b42feca27218a922d0a258ae09d8
danielo <daniel.owen@caplin.com>
2017-03-31 11:52:41 +0100

    upnew API docs for v1.6.0

---
+++ v1.6.0/js/hosted-docs/hosted-docs.js
@@ -0,0 +1,12 @@
+/*
+	This file is intentionally empty, the file is written to by the build when apps are created to be hosted on apidocs.bladerunnerjs.org.
+*/
+
+function addVersion() {
+	var dashboardLinkEl = document.querySelector('.dashboard-link');
+	var versionEl = document.createElement('li');
+	versionEl.innerHTML = '<a href="https://github.com/BladeRunnerJS/brjs/releases/v1.5.9" target="_blank">BRJS v1.5.9-57.gd45669558</a>';
+	dashboardLinkEl.parentNode.insertBefore(versionEl, dashboardLinkEl.nextSibling);
+};
+if (window.addEventListener) window.addEventListener('load', addVersion, false);
+else if (window.attachEvent) window.attachEvent('onload', addVersion);
